64c64d7d01334a36269bfc1cad28ff625c99de3a
Santiago Piedrafita <santiagopiedrafita@gmail.com>
2022-05-31 13:10:39 -0300

    features/ create routes and prettier config

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import EditPage from './pages/EditPage'
+import CreatePage from './pages/CreatePage'
+import NotFoundPage from './pages/NotFoundPage'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/editPage" element={<EditPage />} />
+        <Route path="/createPage" element={<CreatePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
