4212560c676f087b1be427cd8a5f420274ddc773
Santiago Piedrafita <santiagopiedrafita@gmail.com>
2022-06-02 10:47:17 -0300

    feat: add backButton and labels in createPage

--- src/components/CreatePage/TaskContent.jsx
+++ src/components/CreatePage/TaskContent.jsx
@@ -3,10 +3,19 @@ import './TaskContent.css'
 export function TaskContent() {
   return (
     <div className="taskContent">
+      <button type="button" className="backButton">
+        Back
+      </button>
       <form className="formTaskContent">
         <input placeholder="Untitled" className="inputTitle" />
-        <input placeholder="Label" className="inputLabel" />
-        <input placeholder="Due Date" className="inputDate" />
+        <div className="label">
+          <p className="pLabel">Label</p>
+          <input placeholder="Empty" className="inputLabel" />
+        </div>
+        <div className="date">
+          <p className="pDate">Due Date</p>
+          <input placeholder="Empty" className="inputDate" />
+        </div>
         <input placeholder="Add a comment" className="inputComment" />
 
         <button type="button" className="cancelButton">
