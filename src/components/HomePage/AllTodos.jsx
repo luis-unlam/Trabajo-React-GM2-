7b9e3df488e1ab1e3f3eb306c813e24952f122ad
tomisaquilan99 <tomas.saquilan@gm2dev.com>
2022-06-02 20:17:27 -0300

    feat: finish home page design and fix some CSS properties

--- src/components/HomePage/AllTodos.jsx
+++ src/components/HomePage/AllTodos.jsx
@@ -7,23 +7,28 @@ export function AllTodos() {
     <ul className="allTodos">
       {todos.map((todo) => (
         <li className="todos" key={todo.id}>
-          <div className="infoTask">
-            <div className="taskFirstRow">
-              <p>{todo.name}</p>
-              <p>{todo.category}</p>
-            </div>
-            <p className="thirdp">Due date: {todo.date}</p>
-            <p className="lastp">{todo.comment}</p>
+          <div className="inputCheckTodoContainer">
+            <input type="checkbox" className="inputCheckTodo" />
           </div>
-          <div className="taskButtons">
-            <NavLink to="/editPage">
-              <button type="button" className="editButton">
-                <i className="fas fa-edit fa-edit fa-2x" />
+          <div className="completeTask">
+            <div className="infoTask">
+              <div className="taskFirstRow">
+                <p>{todo.name}</p>
+                <p>{todo.category}</p>
+              </div>
+              <p className="thirdp">Due date: {todo.date}</p>
+              <p className="lastp">{todo.comment}</p>
+            </div>
+            <div className="taskButtons">
+              <NavLink to="/editPage">
+                <button type="button" className="editButton">
+                  <i className="fas fa-edit fa-edit fa-2x" />
+                </button>
+              </NavLink>
+              <button type="button" className="deleteButton">
+                <i className="fas fa-trash-alt fa-delete fa-2x" />
               </button>
-            </NavLink>
-            <button type="button" className="deleteButton">
-              <i className="fas fa-trash-alt fa-delete fa-2x" />
-            </button>
+            </div>
           </div>
         </li>
       ))}
--- src/components/HomePage/Categories.jsx
+++ src/components/HomePage/Categories.jsx
@@ -2,11 +2,11 @@ import './Categories.css'
 
 export function Categories() {
   return (
-    <div className="categoriesTask">
+    <nav className="categoriesTask">
       <h4 className="categoriesTask_item">All</h4>
       <h4 className="categoriesTask_item">Groceries</h4>
       <h4 className="categoriesTask_item">College</h4>
       <h4 className="categoriesTask_item">Payments</h4>
-    </div>
+    </nav>
   )
 }
