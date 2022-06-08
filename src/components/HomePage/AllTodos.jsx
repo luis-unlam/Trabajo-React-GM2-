1a5e10e2172f8786babb95a04ae505f6366c597b
tomisaquilan99 <tomas.saquilan@gm2dev.com>
2022-06-08 11:41:47 -0300

    feat: filter todos with search bar (provisional, no optimized)

--- src/components/HomePage/AllTodos.jsx
+++ src/components/HomePage/AllTodos.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import './AllTodos.css'
@@ -8,6 +10,7 @@ const url = 'https://6297eb2e8d77ad6f750aadac.mockapi.io/api/v1/'
 
 export function AllTodos() {
   const [todos, setTodos] = useState([])
+  const [search, setSearch] = useState('')
 
   const obtenerTodos = async () => {
     const data = await fetch(`${url}tasks`)
@@ -44,45 +47,68 @@ export function AllTodos() {
     obtenerTodos()
   }, [])
 
+  const searcher = (e) => {
+    setSearch(e.target.value)
+    console.log(e.target.value)
+  }
+
+  let results = []
+  if (!search) {
+    results = todos
+    console.log(results)
+  } else {
+    results = todos.filter((dato) =>
+      dato.name.toLowerCase().includes(search.toLocaleLowerCase())
+    )
+  }
+
   return (
-    <ul className="allTodos">
-      {todos.map((todo) => (
-        <li className="todos" key={todo.id}>
-          <div className="inputCheckTodoContainer">
-            <input type="checkbox" className="inputCheckTodo" />
-          </div>
-          <div className="completeTask">
-            <div className="infoTask">
-              <div className="taskFirstRow">
-                <p>{todo.name}</p>
-                <span>{todo.category}</span>
+    <div>
+      <input
+        value={search}
+        onChange={searcher}
+        placeholder="Buscar"
+        className="inputTask"
+      />
+      <ul className="allTodos">
+        {results.map((todo) => (
+          <li className="todos" key={todo.id}>
+            <div className="inputCheckTodoContainer">
+              <input type="checkbox" className="inputCheckTodo" />
+            </div>
+            <div className="completeTask">
+              <div className="infoTask">
+                <div className="taskFirstRow">
+                  <p>{todo.name}</p>
+                  <span>{todo.category}</span>
+                </div>
+                <p className="thirdp">Due date: {todo.date}</p>
+                <p className="lastp">{todo.comment}</p>
               </div>
-              <p className="thirdp">Due date: {todo.date}</p>
-              <p className="lastp">{todo.comment}</p>
+              <div className="taskButtons">
+                <NavLink to="/editPage">
+                  <button
+                    type="button"
+                    className="editButton"
+                    onClick={() => onHandleEdit(todo)}
+                  >
+                    <i className="fas fa-edit fa-edit fa-2x" />
+                  </button>
+                </NavLink>
+                <NavLink to="/">
+                  <button
+                    type="submit"
+                    className="deleteButton"
+                    onClick={() => onHandleDelete(todo.id)}
+                  >
+                    <i className="fas fa-trash-alt fa-delete fa-2x" />
+                  </button>
+                </NavLink>
+              </div>
             </div>
-            <div className="taskButtons">
-              <NavLink to="/editPage">
-                <button
-                  type="button"
-                  className="editButton"
-                  onClick={() => onHandleEdit(todo)}
-                >
-                  <i className="fas fa-edit fa-edit fa-2x" />
-                </button>
-              </NavLink>
-              <NavLink to="/">
-                <button
-                  type="submit"
-                  className="deleteButton"
-                  onClick={() => onHandleDelete(todo.id)}
-                >
-                  <i className="fas fa-trash-alt fa-delete fa-2x" />
-                </button>
-              </NavLink>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
