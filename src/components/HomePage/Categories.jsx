85aa32fea2eaf22e3cf209900f731cb1fdb18b0c
Luis Patty <luis.patty.unlam@gmail.com>
2022-06-07 23:45:20 -0300

    feat: create categories dinamics(home page)

--- src/components/HomePage/Categories.jsx
+++ src/components/HomePage/Categories.jsx
@@ -1,12 +1,28 @@
 import './Categories.css'
+import { useEffect, useState } from 'react'
+
+const url = 'https://6297eb2e8d77ad6f750aadac.mockapi.io/api/v1/'
 
 export function Categories() {
+  const [todos, setTodos] = useState([])
+
+  const obtenerTodos = async () => {
+    const data = await fetch(`${url}tasks`)
+    const resultTodos = await data.json()
+    setTodos(resultTodos)
+    // eslint-disable-next-line no-console
+    console.log(resultTodos)
+  }
+
+  useEffect(() => {
+    obtenerTodos()
+  }, [])
+
   return (
     <nav className="categoriesTask">
-      <h4 className="categoriesTask_item">All</h4>
-      <h4 className="categoriesTask_item">Groceries</h4>
-      <h4 className="categoriesTask_item">College</h4>
-      <h4 className="categoriesTask_item">Payments</h4>
+      {todos.map((todo) => (
+        <h4 className="categoriesTask_item">{todo.category}</h4>
+      ))}
     </nav>
   )
 }
